eaea2c38b12ece8a6d2bbff11c3e9e91a55b6197
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-01-22 06:58:22 +0000

    [Add]フラッシュメッセージ、[Update]top

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,6 @@ class BooksController < ApplicationController
   before_action :authenticate_user!
 
 
-  def new
-  end
-
   def index
     @book = Book.new
     @books = Book.all
@@ -14,6 +11,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+     flash[:notice]="You have created book successfully."
      redirect_to book_path(@book.id)
     else
       @user = current_user
@@ -24,8 +22,10 @@ class BooksController < ApplicationController
 
   def destroy
     @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
 
@@ -36,12 +36,22 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+     flash[:notice]="Book was successfully updated"
+     redirect_to book_path(@book.id)
+    else
+     render :edit
+    end
+
   end
 
   private
