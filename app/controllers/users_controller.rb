683c30d6541eb70d98c0c7b5a3f14f3317dad256
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-01-22 10:24:59 +0000

    [Add]部分テンプレート

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
@@ -16,7 +17,11 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = current_user
+    @book = Book.new
   end
+
+
 
   private
   def user_params
