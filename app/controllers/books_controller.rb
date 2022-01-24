b9f8f3a13f61128c91e1cec96fdc9c16797ca3d7
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-01-24 03:11:08 +0000

    [Fixed]レイアウト修正、エラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -63,4 +63,6 @@ class BooksController < ApplicationController
   end
 
 
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as:'about'
 
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update]
+  resources :books, only: [:create, :index, :show, :edit, :destroy, :update]
   resources :users, only: [:show, :edit, :update, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
